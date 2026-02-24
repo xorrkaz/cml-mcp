b6a36ad41f18f19aed653aedc39fc1e73be20e07
Tomas Mikuska <tmikuska@cisco.com>
2026-02-24 18:18:07 +0100

    Unicon support for consoles

---
+++ src/cml_mcp/tools/unicon_cli.py
@@ -0,0 +1,99 @@
+import logging
+import traceback
+
+import yaml
+from unicon import Connection
+
+from cml_mcp.cml_client import CMLClient
+from cml_mcp.cml.simple_webserver.schemas.common import UUID4Type
+from cml_mcp.cml.simple_webserver.schemas.nodes import NodeLabel
+
+logger = logging.getLogger("cml-mcp.tools.unicon_cli")
+
+TERMWS_BINARY = "/usr/local/bin/termws"
+TIMEOUT = 300
+
+
+def unicon_send_cli_command_sync(
+    client: CMLClient,
+    lid: UUID4Type,
+    label: NodeLabel,  # pyright: ignore[reportInvalidTypeForm]
+    commands: str,
+    config_command: bool,
+) -> str:
+    resp = client.vclient._session.get(f"/labs/{lid}/pyats_testbed")
+    pyats_data = yaml.safe_load(resp.text)
+    device_pyats_data = pyats_data["devices"][label]
+
+    resp = client.vclient._session.get(f"/labs/{lid}/nodes", params={"data": True, "operational": True})
+    if resp.status_code != 200:
+        raise Exception("Cannot retrieve node console key. Is the node running?")
+
+    nodes = resp.json()
+    for node in nodes:
+        if node["label"] == label:
+            consoles = node["operational"]["serial_consoles"]
+            console_key = consoles[0]["console_key"]
+            break
+    else:
+        raise Exception("Cannot retrieve node console key. Is the node running?")
+
+    connect_command = f"{TERMWS_BINARY} -host [::1] -port 8006 -internal {console_key}"
+    connection = None
+    error = None
+    try:
+        connection = Connection(
+            hostname=label,
+            start=[connect_command],
+            os=device_pyats_data["os"],
+            series=device_pyats_data.get("series"),  # can be None
+            credentials=device_pyats_data["credentials"],
+            log_stdout=False,
+            log_buffer=True,
+            learn_hostname=True,
+            learn_tokens=False,
+            connection_timeout=10,
+            prompt_recovery=True,
+        )
+        connection.settings.GRACEFUL_DISCONNECT_WAIT_SEC = 0
+        connection.settings.POST_DISCONNECT_WAIT_SEC = 0
+        connection.settings.LEARN_DEVICE_TOKENS = False
+
+        if config_command:
+            result = connection.configure(commands, timeout=TIMEOUT)
+        else:
+            result = connection.execute(commands, timeout=TIMEOUT)
+
+        return result
+    except Exception as exc:
+        error = traceback.TracebackException.from_exception(exc)
+        raise
+    finally:
+        if connection is not None:
+            connection.disconnect()
+
+        _save_connection_log(lid, label, connection, error)
+
+
+def _save_connection_log(
+    lid: UUID4Type,
+    label: NodeLabel,  # pyright: ignore[reportInvalidTypeForm]
+    connection: Connection,
+    error: traceback.TracebackException
+):
+    log_path = f"/tmp/unicon_{lid}_{label}.log"
+    try:
+        # "at" mode: Opens for appending (a) in text mode (t).
+        # It creates the file if it does not exist.
+        with open(log_path, "at") as logfile:
+            logfile.write(f"\n--- Start log of extraction for {label} ---\n")
+            if error:
+                logfile.write("Failed with error:\n")
+                logfile.writelines(error.format())
+            if connection is not None and connection.log_buffer:
+                logfile.write(connection.log_buffer)
+            else:
+                logfile.write("No connection log was retained\n")
+            logfile.write(f"--- End log ---\n")
+    except Exception as exc:
+        logger.exception("Failed to save unicon connection log for %s: %s", label, exc)
